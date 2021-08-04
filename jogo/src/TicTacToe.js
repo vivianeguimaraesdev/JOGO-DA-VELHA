384c77be3d784ec9fc297d68ace00f2eb01e1ea2
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-08-04 13:11:18 -0300

    Create TicTacToe.js

---
+++ jogo/src/TicTacToe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TicTacToe.css';
+
+function TicTacToe() {
+  return (
+    <main>
+    <h1 className="title">Jogo da Velha</h1>
+    </main>
+  );
+}
+
+
+export default TicTacToe;
